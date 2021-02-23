be9d117299efd15490154d7a03213ef03799755c
mubariz <mhdmdssr999@gmail.com>
2021-02-24 03:13:25 +0530

    style error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,19 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+  <Wrapper>
+  <div>
+    <h1>404</h1>
+    <h3>sorry, the page you tried cannot be found</h3>
+    <Link to='/' className='btn'>
+      Return To Home
+    </Link>
+  </div>
+</Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
   place-items: center;
-  background: var(--clr-primary-10);
+  background: #e3e2df;
   text-align: center;
   h1 {
     font-size: 10rem;
   }
   h3 {
-    color: var(--clr-grey-3);
+    color: #e3afbc;
     margin-bottom: 1.5rem;
   }
 `;
